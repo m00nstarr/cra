6addc6f43de769d37dd75220805aa219fcc2aab9
m00nstarr <moonstar11430@gmail.com>
2025-05-02 16:48:14 +0900

    [Test] 테스트 Coverage 향상을 위한 테스트 추가

--- Project1/ALUKATA/alu_test.cpp
+++ Project1/ALUKATA/alu_test.cpp
@@ -43,3 +43,82 @@ TEST_F(ALUFixture, MULTest) {
 	EXPECT_EQ(200, result.getResult());
 	EXPECT_EQ(0, result.getStatus());
 }
+
+TEST_F(ALUFixture, SUBTest) {
+
+	alu.setOperand1(10);
+	alu.setOperand2(20);
+	alu.setOPCODE("SUB");
+
+	alu.enableSignal(&result);
+
+	EXPECT_EQ(-10, result.getResult());
+	EXPECT_EQ(0, result.getStatus());
+}
+
+TEST_F(ALUFixture, MulNoOperand) {
+
+	alu.setOperand2(20);
+	alu.setOPCODE("MUL");
+
+	alu.enableSignal(&result);
+
+	EXPECT_EQ(65535, result.getResult());
+	EXPECT_EQ(1, result.getStatus());
+
+	alu.setOperand2(-1);
+	alu.setOperand1(20);
+
+	alu.enableSignal(&result);
+
+	EXPECT_EQ(65535, result.getResult());
+	EXPECT_EQ(2, result.getStatus());
+}
+
+TEST_F(ALUFixture, AddNoOperand) {
+
+	alu.setOperand2(20);
+	alu.setOPCODE("ADD");
+
+	alu.enableSignal(&result);
+
+	EXPECT_EQ(65535, result.getResult());
+	EXPECT_EQ(1, result.getStatus());
+
+	alu.setOperand1(20);
+	alu.setOperand2(-1);
+
+	alu.enableSignal(&result);
+	EXPECT_EQ(65535, result.getResult());
+	EXPECT_EQ(2, result.getStatus());
+}
+
+TEST_F(ALUFixture, SubNoOperand) {
+
+	alu.setOperand2(20);
+	alu.setOPCODE("SUB");
+
+	alu.enableSignal(&result);
+
+	EXPECT_EQ(65535, result.getResult());
+	EXPECT_EQ(1, result.getStatus());
+
+	alu.setOperand1(20);
+	alu.setOperand2(-1);
+
+	alu.enableSignal(&result);
+	EXPECT_EQ(65535, result.getResult());
+	EXPECT_EQ(2, result.getStatus());
+}
+
+
+TEST_F(ALUFixture, NoOpCode) {
+
+	alu.setOperand1(20);
+	alu.setOperand2(20);
+
+	alu.enableSignal(&result);
+
+	EXPECT_EQ(65535, result.getResult());
+	EXPECT_EQ(3, result.getStatus());
+}
