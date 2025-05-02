4378d4b92fcc7c2b7af7ffc54eb92cef6723541a
m00nstarr <moonstar11430@gmail.com>
2025-05-02 17:02:43 +0900

    [Refactor] 코드 리팩토링

--- Project1/ALUKATA/alu.cpp
+++ Project1/ALUKATA/alu.cpp
@@ -3,72 +3,69 @@
 
 class ALU
 {
-    int operand1 = -1;
-    int operand2 = -1;
-    std::string OPCODE = "";
+	int operand1 = -1;
+	int operand2 = -1;
+	std::string OPCODE = "";
 
 public:
-    void setOperand1(int operand1) {
-        this->operand1 = operand1;
-    }
+	void setOperand1(int operand1) {
+		this->operand1 = operand1;
+	}
 
-    void setOperand2(int operand2) {
-        this->operand2 = operand2;
-    }
+	void setOperand2(int operand2) {
+		this->operand2 = operand2;
+	}
 
-    void setOPCODE(std::string OPCODE) {
-        this->OPCODE = OPCODE;
-    }
+	void setOPCODE(std::string OPCODE) {
+		this->OPCODE = OPCODE;
+	}
 
-    void enableSignal(Result* r) {
-        if (OPCODE == "ADD" && OPCODE != "MUL" && OPCODE != "SUB") {
-            if (operand1 != -1 && operand2 != -1) {
-                int result = operand1 + operand2;
-                r->setResult(result);
-                r->setStatus(0);
-            }
-            else if (operand1 == -1) {
-                r->setResult(65535);
-                r->setStatus(1);
-            }
-            else if (operand2 == -1) {
-                r->setResult(65535);
-                r->setStatus(2);
-            }
-        }
-        else if (OPCODE != "ADD" && OPCODE == "MUL" && OPCODE != "SUB") {
-            if (operand1 != -1 && operand2 != -1) {
-                int result = operand1 * operand2;
-                r->setResult(result);
-                r->setStatus(0);
-            }
-            else if (operand1 == -1) {
-                r->setResult(65535);
-                r->setStatus(1);
-            }
-            else if (operand2 == -1) {
-                r->setResult(65535);
-                r->setStatus(2);
-            }
-        }
-        else if (OPCODE != "ADD" && OPCODE != "MUL" && OPCODE == "SUB") {
-            if (operand1 != -1 && operand2 != -1) {
-                int result = operand1 - operand2;
-                r->setResult(result);
-                r->setStatus(0);
-            }
-            else if (operand1 == -1) {
-                r->setResult(65535);
-                r->setStatus(1);
-            }
-            else if (operand2 == -1) {
-                r->setResult(65535);
-                r->setStatus(2);
-            }
-        }
-        else {
-            r->setResult(65535);
-            r->setStatus(3);
-        }
-    }
+	int calculate(int operand1, int operand2, const std::string& opcode) {
+
+		if (opcode == "ADD") {
+			return operand1 + operand2;
+		}
+		else if (opcode == "SUB") {
+			return operand1 - operand2;
+		}
+		else if (opcode == "MUL") {
+			return operand1 * operand2;
+		}
+	}
+
+
+	void enableSignal(Result* r) {
+
+		if (isValidOperand() == false) {
+			if (operand1 == -1) {
+				r->setResult(65535);
+				r->setStatus(1);
+				return;
+			}
+			r->setResult(65535);
+			r->setStatus(2);
+			return;
+		}
+
+		if (isValidOpcode() == false) {
+			r->setResult(65535);
+			r->setStatus(3);
+
+			return;
+		}
+
+		int result = 0;
+		r->setResult(calculate(operand1, operand2, OPCODE));
+		r->setStatus(0);
+		return;
+	}
+	bool isValidOperand()
+	{
+		return operand1 != -1 && operand2 != -1;
+	}
+
+	bool isValidOpcode()
+	{
+		return OPCODE == "ADD" || OPCODE == "MUL" || OPCODE == "SUB";
+	}
 };
