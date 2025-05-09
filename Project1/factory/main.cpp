24fcd6bb2e8f14ce2d7988f8f0fc53fb1d451574
Hyung-il Kim <hyungil@kaist.ac.kr>
2025-05-09 10:16:13 +0900

    [Feat] 팩토리 메소드 적용

--- Project1/factory/main.cpp
+++ Project1/factory/main.cpp
@@ -15,10 +15,10 @@ public:
 	};
 };
 
-class RedDoll : public Doll {
+class TinyDoll : public Doll {
 public:
 	void push() override {
-		cout << "빨강빨강 인형\n";
+		cout << "Tiny 인형\n";
 	};
 };
 
@@ -29,40 +29,106 @@ public:
 	};
 };
 
+class Shoe {
+public:
+	virtual ~Shoe() {}
+	virtual void wear() = 0;
+};
+
+class TinyShoe : public Shoe {
+public: 
+	void wear() {
+		cout << "tiny Shoe 를 입었습니다\n";
+	}
+};
+
+class BlueShoe : public Shoe {
+public:
+	void wear() {
+		cout << "blue Shoe 를 입었습니다\n";
+	}
+};
+
+class Acc {
+public:
+	virtual ~Acc() {}
+	virtual void put() = 0;
+};
+
+class TinyAcc : public Acc {
+public:
+	void put() {
+		cout << "tiny Acc를 착용하였습니다.\n";
+	}
+};
+
+class BlueAcc : public Acc {
+public:
+	void put() {
+		cout << "blue Acc를 착용하였습니다.\n";
+	}
+};
+
+
 class Creator {
 public:
 	Doll* myDoll;
 
 	void play() {
-		Doll* myDoll = factoryMethod();
+		Doll* myDoll = createDoll();
+		Shoe* myShoe = createShoe();
+		Acc* myAcc = createAcc();
+
 		myDoll->push();
+		myShoe->wear();
+		myAcc->put();
+
+		delete myDoll;
+		delete myShoe;
+		delete myAcc;
 	};
-	virtual Doll* factoryMethod() {
-		return nullptr;
-	};
+
+	virtual Doll* createDoll() = 0;
+	virtual Shoe* createShoe() = 0;
+	virtual Acc* createAcc() = 0;
+
 };
 
 class BlueCreatorBaby : public Creator {
 public:
-	Doll* factoryMethod() override {
+	Doll* createDoll() override {
 		return new BlueDoll();
 	}
-};
-
-class RedCreatorBaby : public Creator {
-public:
-	Doll* factoryMethod() override {
-		return new RedDoll();
+	Shoe* createShoe() override {
+		return new BlueShoe();
 	}
-
+	Acc* createAcc() override {
+		return new BlueAcc();
+	}
 };
 
+class TinyCreatorBaby : public Creator {
+public:
+	Doll* createDoll() override {
+		return new TinyDoll();
+	}
+	Shoe* createShoe() override {
+		return new TinyShoe();
+	}
+	Acc* createAcc() override {
+		return new TinyAcc();
+	}
+};
 
 int main() {
 
 	Creator* creator = new BlueCreatorBaby();
 	creator->play();
+	delete creator;
 
+	creator = new TinyCreatorBaby();
+	creator->play();
+	delete creator;
 
 	return 0;
 }
